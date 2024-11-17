14dd02eafc49a39ac90a1e88b6b483fd6afb9566
jameskinyua590 <jameskinyua590@gmail.com>
2024-11-17 15:08:58 +0000

    set up project structure

---
+++ .idx/dev.nix
@@ -0,0 +1,47 @@
+{ pkgs, ... }: {
+
+    # Which nixpkgs channel to use.
+    channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+    packages = [
+        pkgs.nodejs_22
+    ];
+
+    # Sets environment variables in the workspace
+    env = {
+        SOME_ENV_VAR = "hello";
+    };
+
+    services.docker = {
+        enable = true;
+    };
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+#   idx.extensions = [
+#     "angular.ng-template"
+#   ];
+
+  # Enable previews and customize configuration
+#   idx.previews = {
+#     enable = true;
+#     previews = {
+#       web = {
+#         command = [
+#           "npm"
+#           "run"
+#           "start"
+#           "--"
+#           "--port"
+#           "$PORT"
+#           "--host"
+#           "0.0.0.0"
+#           "--disable-host-check"
+#         ];
+#         manager = "web";
+#         # Optionally, specify a directory that contains your web app
+#         # cwd = "app/client";
+#       };
+#     };
+#   };
+}
