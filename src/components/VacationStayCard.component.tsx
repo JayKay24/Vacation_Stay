ae5c4a8c849397ccaf423b7bef81cc94694d3c5a
jameskinyua590 <jameskinyua590@gmail.com>
2024-11-18 10:51:06 +0000

    add unit test for VacationStayCard.component

--- src/components/VacationStayCard.component.tsx
+++ src/components/VacationStayCard.component.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { VacationStayFiltered } from '../app_types/vacation_stay';
 
 const VacationStayCard: React.FC<{
-    stay: VacationStayFiltered
-}> = ({ stay }) => {
+    stay: VacationStayFiltered,
+    testId: string
+}> = ({ stay, testId = 'not-set' }) => {
     return (
-        <div>
+        <div data-testId={testId}>
             <h2>{stay.name}</h2>
             <h3>{stay.description}</h3>
             <div>{stay._count.reviews}</div>
---
+++ src/components/VacationStayCard.rendering.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+
+import VacationStayData from '../utils/mock-response-multiple.json';
+import VacationStayCard from './VacationStayCard.component';
+import { VacationStayFiltered } from '../app_types/vacation_stay';
+
+describe('VacationStayCard.component: rendering', () => {
+    it('renders a vacation title correctly', () => {
+        const testID = 'unit-test-vacation';
+
+        render(<VacationStayCard testId={testID} stay={VacationStayData[0] as VacationStayFiltered} />);
+
+        const card = screen.getByTestId(testID);
+
+        expect(card).not.toBeNull();
+
+        const children = card.children;
+        expect(children.length).toBeGreaterThan(2);
+        expect(children.item(0)?.innerHTML).toContain(VacationStayData[0].name);
+    });
+}); 
