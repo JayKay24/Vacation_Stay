54d9e204c9df85a02a619c06b41e0fb07c682ef3
jameskinyua590 <jameskinyua590@gmail.com>
2024-11-18 12:01:16 +0000

    import React

--- src/components/VacationStayCard.rendering.test.tsx
+++ src/components/VacationStayCard.rendering.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import VacationStayData from '../utils/mock-response-multiple.json';
--- src/pages/DisplayVacationStay.tsx
+++ src/pages/DisplayVacationStay.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import useHTTPClient from '../hooks/useHTTPClient';
 import { APIResponseVacationStayComplete } from '../app_types/vacation_stay';
