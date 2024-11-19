89e6396fa9cf41737353f925e5578188384d9585
jameskinyua590 <jameskinyua590@gmail.com>
2024-11-19 16:07:49 +0000

    update passing props into render

--- src/components/CompleteAddress.rendering.test.tsx
+++ src/components/CompleteAddress.rendering.test.tsx
@@ -4,13 +4,13 @@ import '@testing-library/jest-dom';
 
 import VacationStayData from '../utils/mock-response-single.json';
 import CompleteAddress from './CompleteAddress.component';
-import { VacationStayComplete } from '../app_types/vacation_stay';
+import { AddressComplete } from '../app_types/vacation_stay';
 
 describe('CompleteAddress.component: rendering', () => {
     it('renders a vacation title correctly', () => {
         const testID = 'unit-test-vacation';
 
-        render(<CompleteAddress testid={testID} address={VacationStayData.data.address as unknown as VacationStayComplete} />);
+        render(<CompleteAddress testid={testID} address={VacationStayData.data.address as unknown as AddressComplete} />);
 
         const filteredAddress = screen.getByTestId(testID);
 
--- src/components/FilteredAddress.rendering.test.tsx
+++ src/components/FilteredAddress.rendering.test.tsx
@@ -4,13 +4,13 @@ import '@testing-library/jest-dom';
 
 import VacationStayData from '../utils/mock-response-multiple.json';
 import FilteredAddress from './FilteredAddress.component';
-import { VacationStayFiltered } from '../app_types/vacation_stay';
+import { AddressFiltered } from '../app_types/vacation_stay';
 
 describe('FilteredAddress.component: rendering', () => {
     it('renders a vacation title correctly', () => {
         const testID = 'unit-test-vacation';
 
-        render(<FilteredAddress testid={testID} address={VacationStayData.data[0].address as unknown as VacationStayFiltered} />);
+        render(<FilteredAddress testid={testID} address={VacationStayData.data[0].address as unknown as AddressFiltered} />);
 
         const filteredAddress = screen.getByTestId(testID);
 
